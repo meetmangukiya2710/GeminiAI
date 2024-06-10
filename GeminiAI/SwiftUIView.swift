1403e58099e2d09d511d9bf3d059317f00b6edce
meetmangukiya2710 <meetmangukiya104@gmail.com>
2024-06-10 15:09:42 +0530

    Logo Chage

---
+++ GeminiAI/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  GeminiAI
+//
+//  Created by R95 on 10/06/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
