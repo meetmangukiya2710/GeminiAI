d4b38e220049f3fac2c4da0d725d1298c9e55568
meetmangukiya2710 <meetmangukiya104@gmail.com>
2024-06-13 12:11:05 +0530

    Login Page

---
+++ GeminiAI/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  GeminiAI
+//
+//  Created by R95 on 12/06/24.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
